b607c043db6f2303351544ec862de10853c48734
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-05-18 18:50:17 +0200

    Post method body

--- src/MySpot.Api/Controllers/ReservationsController.cs
+++ src/MySpot.Api/Controllers/ReservationsController.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using MySpot.Api.Models;
 
 namespace MySpot.Api.Controllers;
 
 [Route("reservations")]
 public class ReservationsController : ControllerBase
 {
+    private static string[] _parkingSpotNames = {"P1", "P2", "P3", "P4", "P5"};
+    private readonly List<Reservation> _reservations = new();
+
+    private static int Id = 1;
+
     [HttpGet]
     public void Get()
     {
@@ -12,11 +18,31 @@ public class ReservationsController : ControllerBase
     }
 
     [HttpPost]
-    public void Post()
+    public void Post([FromBody] Reservation reservation)
     {
-        
+        reservation.Id = Id;
+        reservation.Date = DateTime.Now.AddDays(1).Date;
+
+
+        if (_parkingSpotNames.All(x => x != reservation.ParkingSpotName))
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            return;
+        }
+
+        var reservationAlreadyExists = _reservations.Any(x => x.Date.Date == reservation.Date.Date
+                                                              && x.ParkingSpotName == reservation.ParkingSpotName);
+
+        if (reservationAlreadyExists)
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            return;
+        }
+
+        Id++;
+        _reservations.Add(reservation);
     }
-    
+
     [HttpPut]
     public void Put()
     {
--- src/MySpot.Api/Models/Reservation.cs
+++ src/MySpot.Api/Models/Reservation.cs
@@ -3,7 +3,7 @@ namespace MySpot.Api.Models;
 public class Reservation
 {
     public int Id { get; set; }
-    public string EmmployeeName { get; set; }
+    public string EmployeeName { get; set; }
     public string ParkingSpotName { get; set; }
     public string LicencePlate { get; set; }
     public DateTime Date { get; set; }
