c0879ccfa1f5839c97bbde4fd0f13e20b3808214
AdrianBabieczko <adrian.babieczko@gmail.com>
2022-06-06 17:59:32 +0200

    Add new exception type, new value objects

---
+++ src/MySpot.Api/Exceptions/InvalidEntityIdException.cs
@@ -0,0 +1,9 @@
+namespace MySpot.Api.Exceptions;
+
+public class InvalidEntityIdException: CustomException
+{
+    public object Id { get;}
+
+    public InvalidEntityIdException(object id) : base($"Cannot set: {id} as entity identifier.")
+        => Id = id;
+}
