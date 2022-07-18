844c32403d73c6297890a318c6a11fa65c66482b
AdrianBabieczko <adrian.babieczko@gmail.com>
2022-07-18 19:21:00 +0200

    Add service collections tests

--- tests/MySpot.Tests.Unit/Framework/ServiceCollectionTests.cs
+++ tests/MySpot.Tests.Unit/Framework/ServiceCollectionTests.cs
@@ -11,26 +11,41 @@ public class ServiceCollectionTests
     public void test()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddScoped<IMessenger, Messenger>();
+        serviceCollection.AddScoped(typeof(IMessenger<>), typeof(Messenger<>));
 
         var serviceProvider = serviceCollection.BuildServiceProvider();
-        
-        var messenger1 = serviceProvider.GetService<IMessenger>();
-        var messenger2 = serviceProvider.GetService<IMessenger>();
-        
-        messenger1.ShouldBe(messenger2);
+        var messenger1 = serviceProvider.GetService<IMessenger<string>>();
+        var messenger2 = serviceProvider.GetService<IMessenger<int>>();
+        messenger1.ShouldNotBeNull();
+        messenger2.ShouldNotBeNull();
+
+        // using(var scope1 = serviceProvider.CreateScope())
+        // {
+        //     var messenger1 = scope1.ServiceProvider.GetService<IMessenger>();
+        //     var messenger2 = scope1.ServiceProvider.GetService<IMessenger>();
+        //
+        //     messenger1.ShouldBe(messenger2);     
+        // }
+        //
+        // using(var scope2 = serviceProvider.CreateScope())
+        // {
+        //     var messenger1 = scope2.ServiceProvider.GetService<IMessenger>();
+        //     var messenger2 = scope2.ServiceProvider.GetService<IMessenger>();
+        //
+        //     messenger1.ShouldBe(messenger2);     
+        // }
     }
 
-    private interface IMessenger
+    private interface IMessenger<in T>
     {
-        void Send();
+        void Send(T message);
     }
 
-    private class Messenger : IMessenger
+    private class Messenger<T> : IMessenger<T>
     {
         private readonly Guid _id = Guid.NewGuid();
         
-        public void Send()
+        public void Send(T message)
         {
             Console.WriteLine($"Sending a message... [ID] {_id}");
         }
