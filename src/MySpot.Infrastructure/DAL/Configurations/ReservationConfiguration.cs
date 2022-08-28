aec4435a202277311df83109cafd6e84be5b1161
AdrianBabieczko <adrian.babieczko@gmail.com>
2022-08-28 07:16:43 +0200

    Add entity type configuration

--- src/MySpot.Infrastructure/DAL/Configurations/ReservationConfiguration.cs
+++ src/MySpot.Infrastructure/DAL/Configurations/ReservationConfiguration.cs
@@ -12,5 +12,14 @@ internal sealed class ReservationConfiguration :  IEntityTypeConfiguration<Reser
         builder.HasKey(x => x.Id);
         builder.Property(x => x.Id)
             .HasConversion(x => x.Value, x => new ReservationId(x));
+        builder.Property(x => x.EmployeeName)
+            .IsRequired()
+            .HasConversion(x => x.Value, x => new EmployeeName(x));
+        builder.Property(x => x.LicencePlate)
+            .IsRequired()
+            .HasConversion(x => x.Value, x => new LicencePlate(x));
+        builder.Property(x => x.Date)
+            .IsRequired()
+            .HasConversion(x => x.Value, x => new Date(x));
     }
-}
+} 
